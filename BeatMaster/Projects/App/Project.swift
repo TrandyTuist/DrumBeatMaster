b5675cb8bef06f422f14df008a203dc09c76f687
Roy-wonji <shuwj81@daum.net>
2024-02-04 11:06:08 +0900

    Revert "✨[feat]:  spm수정"
    This reverts commit 41bd9b81672db7f9b3a63c16508ebe9725dd0677.

--- BeatMaster/Projects/App/Project.swift
+++ BeatMaster/Projects/App/Project.swift
@@ -19,7 +19,8 @@ let project = Project.makeAppModule(
     dependencies: [
         .Core(implements: .Core),
         .Shared(implements: .Shareds),
-        .Networking(implements: .Networkings)
+        .Networking(implements: .Networkings),
+        .SPM.Supabase
         
         
     
