f0d49e271703dc16a68fbe8fb102c8997fd9f77b
Roy <shuwj81@daum.net>
2024-02-19 11:49:12 +0900

    ➕[add]:  컬러 및 이미지 추가 및  텍스트 애니메이션 추가 #1
     - 컬러 추가 및  텍스트 애니메이션 파일 생성 해서 추가

--- BeatMaster/Projects/Shared/DesignSystem/Sources/UI/TextAnimation/BounceAnimationView.swift
+++ BeatMaster/Projects/Shared/DesignSystem/Sources/UI/TextAnimation/BounceAnimationView.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+import SwiftUI
+
+public struct BounceAnimationView: View {
+    let characters: Array<String.Element>
+    let fontSize: CGFloat
+    
+    @State var offsetYForBounce: CGFloat = -50
+    @State var opacity: CGFloat = 0
+    @State var baseTime: Double
+    
+    public init(
+        text: String,
+        startTime: Double,
+        fontSize: CGFloat
+    ){
+        self.characters = Array(text)
+        self.baseTime = startTime
+        self.fontSize = fontSize
+    }
+    
+    public var body: some View {
+        HStack(spacing:0){
+            ForEach(0..<characters.count) { num in
+                Text(String(self.characters[num]))
+                    .pretendardFont(family: .Bold, size: fontSize)
+                    .offset(x: 0.0, y: offsetYForBounce)
+                    .opacity(opacity)
+                    .animation(.spring(response: 0.2, dampingFraction: 0.5, blendDuration: 0.1).delay( Double(num) * 0.1 ), value: offsetYForBounce)
+            }
+            .onTapGesture {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.0) {
+                    opacity = 0
+                    offsetYForBounce = -50
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                    opacity = 1
+                    offsetYForBounce = 0
+                }
+            }
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now() + (0.8 + baseTime)) {
+                    opacity = 1
+                    offsetYForBounce = 0
+                }
+            }
+        }
+    }
+}
