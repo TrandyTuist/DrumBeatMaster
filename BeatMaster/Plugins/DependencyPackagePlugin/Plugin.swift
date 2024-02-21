f4e0ff9733c87f5ea7e922ded37f0f32f77d2b5b
Roy <shuwj81@daum.net>
2024-02-21 15:29:49 +0900

    ✨[feat]: 라이브러리 관련 package 플러그인 추가 #5

---
+++ BeatMaster/Plugins/DependencyPackagePlugin/Plugin.swift
@@ -0,0 +1,10 @@
+//
+//  Plugin.swift
+//  Plugins
+//
+//  Created by 서원지 on 2/21/24.
+//
+
+import ProjectDescription
+
+let plugin = Plugin(name: "DependencyPackagePlugin")
---
+++ BeatMaster/Plugins/DependencyPackagePlugin/ProjectDescriptionHelpers/DependencyPackage/Extension+TargetDependency.swift
@@ -0,0 +1,21 @@
+//
+//  Extension+TargetDependency.swift
+//  Plugins
+//
+//  Created by 서원지 on 2/21/24.
+//
+
+import ProjectDescription
+
+public extension TargetDependency {
+    enum SPM {}
+}
+
+public extension TargetDependency.SPM {
+    static let Moya = TargetDependency.external(name: "Moya", condition: .none)
+    static let CombineMoya = TargetDependency.external(name: "CombineMoya", condition: .none)
+    static let ComposableArchitecture = TargetDependency.external(name: "ComposableArchitecture", condition: .none)
+    static let Swinject = TargetDependency.external(name: "Swinject", condition: .none)
+    static let Supabase = TargetDependency.external(name: "Supabase", condition: .none)
+}
+
