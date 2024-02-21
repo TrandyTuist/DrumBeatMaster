ba2ecb33d948527a6e9caa732feddaad97a8652a
Roy <shuwj81@daum.net>
2024-02-21 15:30:04 +0900

    ✨[feat]: 모듈 관련 플러그인 추가 #5

---
+++ BeatMaster/Plugins/DependencyPlugin/ProjectDescriptionHelpers/TargetDependency+Module/Modules.swift
@@ -0,0 +1,78 @@
+//
+//  Modules.swift
+//  Plugins
+//
+//  Created by 서원지 on 2/21/24.
+//
+
+import Foundation
+import ProjectDescription
+
+public enum ModulePath {
+    case Feature(Features)
+    case Core(Cores)
+    case Networking(Networkings)
+    case Shared(Shareds)
+}
+
+//MARK: -  앱  모듈
+public extension ModulePath {
+    enum App: String, CaseIterable {
+        case iOS
+        case iPad
+        
+        public static let name: String = "App"
+    }
+}
+
+// MARK: FeatureModule
+public extension ModulePath {
+    enum Features: String, CaseIterable {
+        case Auth
+        case BPMCounter
+        case Metronome
+        case Splash
+        case Root
+        
+        public static let name: String = "Feature"
+    }
+}
+
+//MARK: -  CoreMoudule
+public extension ModulePath {
+    enum Cores: String, CaseIterable {
+        case Core
+        case Authorization
+        case Station
+        
+        public static let name: String = "Core"
+    }
+}
+
+//MARK: -  CoreDomainModule
+public extension ModulePath {
+    enum Networkings: String, CaseIterable {
+        case Networking
+        case Networkings
+        case Model
+        case Service
+        case DiContainer
+        case ThirdPartys
+        
+        
+        public static let name: String = "Networking"
+    }
+}
+
+public extension ModulePath {
+    enum Shareds: String, CaseIterable {
+        case Shareds
+        case DesignSystem
+        case Constants
+        case ThirdParty
+        
+        public static let name: String = "Shared"
+    }
+}
+
+
