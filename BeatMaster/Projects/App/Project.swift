7ca65fe53f513f39566013b253a769824c41ce71
Roy <shuwj81@daum.net>
2024-03-25 23:15:18 +0900

    🪛[chore]: 카카오 로그인 로그인 수정 및 라이브러리 추가시 Makefile [#7]
    * Makefile 에 cache 추가

--- BeatMaster/Projects/App/Project.swift
+++ BeatMaster/Projects/App/Project.swift
@@ -23,7 +23,7 @@ let project = Project.makeAppModule(
         
     ],
     sources: ["Sources/**", "Resources/FontAsset/**"],
-    resources: ["Resources/**"],
+    resources: ["Resources/**", "../Support/AuthKey_P552TDR3Q2.p8", "../Support/PrivacyInfo.xcprivacy"],
     infoPlist: .file(path: "../Support/Info.plist"),
     entitlements: .file(path: "../Entitlements/BeatMaster.entitlements")
     
